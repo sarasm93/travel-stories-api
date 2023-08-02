a3a4fcb8923dc52c98bd2227fee9cf75c08f4c64
Sara Smith <saraa.smith@hotmail.com>
2023-08-02 17:38:07 +0000

    Fix typo in jwt auth refresh cookie setting

--- travel_stories_api/settings.py
+++ travel_stories_api/settings.py
@@ -98,7 +98,7 @@ CORS_ALLOW_CREDENTIALS = True
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_REFRESH_COOKE = 'my-refresh-token'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 ROOT_URLCONF = 'travel_stories_api.urls'
