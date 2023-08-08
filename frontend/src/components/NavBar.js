e192f5fc969c3178f80b20b184dda33e1899e4ed
Sara Smith <saraa.smith@hotmail.com>
2023-08-08 20:40:48 +0000

    Finish styling of signup form

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import styles from "../styles/NavBar.module.css";
+import navStyles from "../styles/NavBar.module.css";
 import logo from "../assets/logo.png";
 import Image from 'react-bootstrap/Image';
 import { NavLink } from "react-router-dom";
@@ -11,8 +11,8 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <Container>
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
-            <NavLink className={styles.NavLogo} to="/">
+        <Navbar className={`${navStyles.NavBar} shadow-sm`} expand="md" fixed="top">
+            <NavLink className={navStyles.NavLogo} to="/">
                 <Image roundedCircle src={logo} alt="logo" height="45" /><Navbar.Brand>  Travel Stories</Navbar.Brand>
             </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -21,18 +21,18 @@ const NavBar = () => {
                     <NavLink 
                         exact
                         to="/" 
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
+                        className={navStyles.NavLink}
+                        activeClassName={navStyles.Active}
                     >Home</NavLink>
                     <NavLink 
                         to="/login"
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
+                        className={navStyles.NavLink}
+                        activeClassName={navStyles.Active}
                     >Log In</NavLink>
                     <NavLink 
                         to="/signup"
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
+                        className={navStyles.NavLink}
+                        activeClassName={navStyles.Active}
                     >Sign Up</NavLink>
                 </Nav>
             </Navbar.Collapse>
--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignUpInForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+
 
 const SignUpForm = () => {
+
+    const [errors, setErrors] = useState({});
+
     return (
         <Row className={styles.Row}>
             <Col className="m-auto py-2 p-md-2" md={6}>
@@ -19,19 +23,29 @@ const SignUpForm = () => {
                             <Form.Control
                                 className={styles.Input}
                                 type="text"
-                                placeholder="Enter a username"
+                                placeholder="Enter username"
                                 name="username"
                             />
                         </Form.Group>
+                        {errors.username?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                        ))}
                         <Form.Group controlId="password1">
                             <Form.Label className="d-none">Password:</Form.Label>
                             <Form.Control
                                 className={styles.Input}
                                 type="password"
-                                placeholder="Enter a password"
+                                placeholder="Enter password"
                                 name="password1"
                             />
                         </Form.Group>
+                        {errors.password1?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                        ))}
                         <Form.Group controlId="password2">
                             <Form.Label className="d-none">Confirm the password:</Form.Label>
                             <Form.Control
@@ -41,12 +55,24 @@ const SignUpForm = () => {
                                 name="password2"
                             />
                         </Form.Group>
-                        <Button
-                            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-                            type="submit"
-                        >
-                            Sign up
-                        </Button>
+                        {errors.password2?.map((message, idx) => (
+                        <Alert variant="warning" key={idx}>
+                            {message}
+                        </Alert>
+                        ))}
+                        <div className="text-center">
+                            <Button
+                                className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                                type="submit"
+                            >
+                                Sign up
+                            </Button>
+                            {errors.non_field_errors?.map((message, idx) => (
+                            <Alert key={idx} variant="warning" className="mt-3">
+                                {message}
+                            </Alert>
+                            ))}
+                        </div>
                     </Form>
                 </Container>
                 <Container className='mt-3'>
