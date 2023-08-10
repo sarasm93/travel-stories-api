8012efcfbf273933995711e0c07b4728102c77a6
Sara Smith <saraa.smith@hotmail.com>
2023-08-10 12:05:51 +0000

    Add changes to api settings, urls and to frontend axios to prepare for deployment

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,11 +1,6 @@
- // IMPORTANT!!
- // Because this React app is running in the same workspace as the API,
- // there is no need to set a separate baseURL until you reach deployment.
- // Setting a baseURL before you reach deployment will cause errors
-
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://travel-stories-api-dfd870d3ac32.herokuapp.com/";
+axios.defaults.baseURL = "/api";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
